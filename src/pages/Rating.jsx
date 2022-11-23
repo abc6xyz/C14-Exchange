ca653e9846add939dd9edc5be058c8a314001121
abc6xyz <byte11101100@gmail.com>
2022-11-24 00:47:23 +0530

    C14_Exch5

--- src/pages/Rating.jsx
+++ src/pages/Rating.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
   ContentCenter,
   ContentRight,
@@ -7,7 +8,7 @@ import {
   TableFiatTransfers, 
   Table
 } from '../components';
-
+import { Button } from '../components/';
 import useMediaQuery from '../hooks/useMediaQuery';
 
 const Rating = () => {
@@ -26,28 +27,33 @@ const Rating = () => {
         </tr>
         <tr>
           <td style={{color: 'green', fontWeight: 'bold'}}>Jharkhand Carbon Offsets</td>
-          <td>5</td>
+          <td>5.89</td>
           <td>5,170</td>
+          <Button size="small">Buy for 50 C14 Tokens</Button>
         </tr>
         <tr>
 <td style={{color: 'green', fontWeight: 'bold'}}>Jhabua Carbon Offsets</td>
-          <td>9</td>
+          <td>9.23</td>
           <td>9,570</td>
+          <Button size="small">Buy for 65 C14 Tokens</Button>
         </tr>
         <tr>
           <td style={{color: 'green', fontWeight: 'bold'}}>Zimbabwe Forestry Project</td>
           <td>11.53</td>
           <td>10,000</td>
+          <Button size="small">Buy for 75 C14 Tokens</Button>
         </tr>
         <tr>
 <td style={{color: 'green', fontWeight: 'bold'}}>Nigeria Forestry Project</td>
-          <td>11.53</td>
+          <td>17.34</td>
           <td>10,000</td>
+          <Button size="small">Buy for 90 C14 Tokens</Button>
         </tr>
         <tr>
 <td style={{color: 'green', fontWeight: 'bold'}}>USA Solar Power Project</td>
-          <td>11.53</td>
+          <td>21.53</td>
           <td>10,000</td>
+          <Button size="small">Buy for 99 C14 Tokens</Button>
         </tr>
         </Table>
     </Section>
